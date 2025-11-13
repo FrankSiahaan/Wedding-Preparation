875b16f4528fe12d2f3b408b02b6d33d5398f3b0
Earlysembiring <nathaliasembiring06@gmail.com>
2025-11-13 14:32:55 +0700

    kata kata

--- public/index.php
+++ public/index.php
@@ -16,4 +16,12 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('vendor.pembayaran');
+});
+
+Route::get('/alamat', function () {
+    return view('vendor.alamat_pengiriman');
+});
+
+Route::get('/checkout', function () {
+    return view('vendor.checkout');
+});
+
+Route::get('/konfirmasi', function () {
+    return view('vendor.pembayaran_berhasil');
 });
